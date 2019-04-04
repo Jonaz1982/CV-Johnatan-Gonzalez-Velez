3b2edaa7f505cc429be5dfcb42df51b5d9b3d772
Johnatan Gonzalez Velez <ingegonzalez1982@gmail.com>
2019-04-04 13:06:59 -0500

    Hoja de vida

---
+++ script.js
@@ -0,0 +1,139 @@
+
+data = {
+    id:5,
+    name:"juan"
+}
+
+fetch("https://randomuser.me/api/",{
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+        "Content-Type": "application/json",
+        // "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+}).then((value) =>{
+    return value.json();
+}).then((value) => {
+    return value.results[0]
+}).then((value) => {
+    let nombre = document.getElementById("nombre");
+    nombre.innerHTML = value.name.title + " " + value.name.first + " " + value.name.last;
+})
+
+
+
+let about = document.getElementById("about");
+let getAbout = document.getElementById("getAbout");
+let resume = document.getElementById("resume");
+let getResume = document.getElementById("getResume");
+let works = document.getElementById("works");
+let getWorks = document.getElementById("getWorks");
+let contact = document.getElementById("contact");
+let getContact = document.getElementById("getContact");
+
+function remove() {
+    about.classList.remove('view');
+    getAbout.classList.remove('selected');
+    resume.classList.remove('view');
+    getResume.classList.remove('selected');
+    works.classList.remove('view');
+    getWorks.classList.remove('selected');
+    contact.classList.remove('view');
+    getContact.classList.remove('selected');
+}
+
+getAbout.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove('about');
+        about.classList.add('view');
+        getAbout.classList.add('selected');
+    }
+
+});
+getResume.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove();
+        resume.classList.add('view');
+        getResume.classList.add('selected');
+    }
+})
+getWorks.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove();
+        works.classList.add('view');
+        getWorks.classList.add('selected');
+    }
+})
+getContact.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove();
+        contact.classList.add('view');
+        getContact.classList.add('selected');
+    }
+
+})
+
+var email = document.getElementById("email");
+var form = document.getElementById("form");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I expect an e-mail, darling!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+  //buttons
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById("myBtn");
+  var span = document.getElementsByClassName("close")[0];
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  var modal1 = document.querySelector('#myModal1');
+  var btn1 = document.getElementById("myBtn1");
+  var span1 = document.getElementsByClassName("close")[1];
+  btn1.onclick = function() {
+    modal1.style.display = "block";
+  }
+  span1.onclick = function() {
+    modal1.style.display = "none";
+  }
+
+  var modal2 = document.querySelector('#myModal2');
+  var btn2 = document.getElementById("myBtn2");
+  var span2 = document.getElementsByClassName("close")[2];
+  btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+  span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+  var modal3 = document.querySelector('#myModal3');
+  var btn3 = document.getElementById("myBtn3");
+  var span3 = document.getElementsByClassName("close")[3];
+  btn3.onclick = function() {
+    modal3.style.display = "block";
+  }
+  span3.onclick = function() {
+    modal3.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
